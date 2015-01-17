1ae21ac82f393ec545d9bd039e231bc2c39b905b
Adam Kosik <edemkee@gmail.com>
2015-01-17 21:39:55 +0100

    simple login interface
    nothing special.
    Note:
    migrate, seed!

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,25 +11,25 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		$this->call('CompetitionsTableSeeder');
 		$this->call('UsersTableSeeder');
+		$this->call('CompetitionsTableSeeder');
 	}
 
-}
-
-class CompetitionsTableSeeder extends Seeder {
-	public function run() {
-		DB::table('competitions')->delete();
-
-		Competition::create(array('name' => "Bicske Kupa 2015", 'description' => "Bicske Kupa 2015", 'date' => "2015/04/11", 'registration_deadline' => "2015/04/04"));
-	}
 }
 
 class UsersTableSeeder extends Seeder {
 	public function run() {
 		DB::table('users')->delete();
 
-		User::create(array('email' => "adicica69satan@citromail.hu", 'password' => md5("youmadfaggot"), 'isadmin' => 1));
-		User::create(array('email' => "adicica70satan@citromail.hu", 'password' => md5("youmadfaggot"), 'isadmin' => 0));
+		User::create(array('email' => "adicica69satan@citromail.hu", 'password' => Hash::make("youmadfaggot"), 'isadmin' => 1, 'remember_token' => 100));
+		User::create(array('email' => "adicica70satan@citromail.hu", 'password' => Hash::make("youmadfaggot"), 'isadmin' => 0, 'remember_token' => 100));
+	}
+}
+
+class CompetitionsTableSeeder extends Seeder {
+	public function run() {
+		DB::table('competitions')->delete();
+
+		Competition::create(array('name' => "Bicske Kupa 2015", 'description' => "Bicske Kupa 2015", 'date' => "2015/04/11", 'registration_deadline' => "2015/04/04", 'user_id' => 1));
 	}
 }
--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,9 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+// route to show the login form
+Route::get('login', array('uses' => 'HomeController@showLogin'));
+
+// route to process the form
+Route::post('login', array('uses' => 'HomeController@doLogin'));
