a3fb31528531e07ca0ecf835e8ece17efc8e4880
Gyula Krajczar <gyula.krajczar@gmail.com>
2015-02-13 11:41:01 +0100

    Clean up routes

--- app/routes.php
+++ app/routes.php
@@ -12,10 +12,7 @@
 */
 
 
-// route to show the login form
 Route::get('login', array('uses' => 'AuthController@showLogin'));
-
-// route to process the form
 Route::post('login', array('uses' => 'AuthController@doLogin'));
 
 Route::get('logout', array('uses' => 'AuthController@doLogout'));
@@ -35,31 +32,7 @@ Route::group(["before" => "auth"], function() {
 	Route::get('registration', 'RegistrationController@show');
 	Route::post('registration/save', 'RegistrationController@update');
 
-	/*
-	// Competition events
-	Route::post('event/save', 'CompetitionEventController@saveEvent');
-	Route::get('event/new', 'CompetitionEventController@newEvent');
-	Route::get('event/{eventId}', 'CompetitionEventController@showEvent');
-	Route::get('event/{eventId}/edit', 'CompetitionEventController@editEvent');
-	Route::get('event/{eventId}/delete', 'CompetitionEventController@deleteCompetition');
-
-	// Teams
-	Route::get('teams', 'TeamController@listTeams');
-	Route::post('team/save', 'TeamController@saveTeam');
-	Route::get('team/new', 'TeamController@newTeam');
-	Route::get('team/{teamId}', 'TeamController@showTeam');
-	Route::get('team/{teamId}/edit', 'TeamController@editTeam');
-	Route::get('team/{teamId}/delete', 'TeamController@deleteTeam');
-
-	// People
-	Route::post('person/save', 'PersonController@savePerson');
-	Route::get('person/new', 'PersonController@newPerson');
-	Route::get('person/{personId}', 'PersonController@showPerson');
-	Route::get('person/{personId}/edit', 'PersonController@editPerson');
-	Route::get('person/{personId}/delete', 'PersonController@deletePerson');
-	*/
-
-	// Delete
+	// Deletion
 	Route::post('delete', 'DeleteController@confirmDelete');
 
 });
